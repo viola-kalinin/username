2673d60a23b17e9235db0747f21c36b748799b33
Viola Kalinin <viola.kalinin@gmail.com>
2020-11-09 10:44:03 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# username.sh
+# Viola Kalinin 
+echo "Enter a username: "
+echo "Use lowercase letters, digits, and underscores"
+echo "It must start with lowercase character"
+echo "It must contain between 3 and 12 characters"
+read NAME
+while echo "$NAME" | egrep -v "^[3-12]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username!"
+	echo "Enter a username: "
+	read NAME
+done
+echo "Thank you"
